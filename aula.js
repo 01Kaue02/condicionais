201ad3715d9599538e3394aaaa3dcade3b606694
Kaue Lima <kauelyma@gmail.com>
2026-03-31 20:40:54 -0300

    else if 5

--- aula.js
+++ aula.js
@@ -1,10 +1,12 @@
-const numero =0;
+const numero =5;
 const NumeroDivisivel = numero % 5 === 0;
 
 console.log(NumeroDivisivel)
 
 if (numero === 0 ){
     console.log("O número é zero.");
+} else if (numero === 5){
+    console.log("O número é invalido.");
 }
  else if  (NumeroDivisivel) {
     console.log("Sim");
